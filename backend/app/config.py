8b7017d73169e119088159c73e9ca0f6d0b34006
kihaas <qwkihaas@gmail.com>
2026-02-11 22:01:09 +0400

    feat: base

---
+++ backend/app/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv(dotenv_path='.env')
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable not set")
---
+++ backend/app/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.testing.plugin.plugin_base import config
+
+from config import DATABASE_URL
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
